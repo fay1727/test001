a7783f5bce863a8b4cb7964e81bf320143686b69
fay1727 <fay1727@qq.com>
2018-05-27 16:27:49 +0800

    描述

--- scripts/test_haha.py
+++ scripts/test_haha.py
@@ -1,5 +1,14 @@
+import allure
 import  pytest
+
 class Test_haha:
+    @allure.step(title="测试名字2")
     @pytest.mark.parametrize("a",[1,2,3])
     def test01(self,a):
+        allure.attach("描述","测试步骤描述")
         assert a !=2
+
+    @allure.step(title="测试名字2")
+    @pytest.mark.parametrize("a", [1, 2, 3])
+    def test01(self, a):
+        assert a != 2
